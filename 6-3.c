72389838c0a5ede5124bfa64f3e387094fb72cf5
ZTao <161002522+Uderwood-TZ@users.noreply.github.com>
2025-05-04 23:56:44 +0800

    Add files via upload

---
+++ 6-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void fun(int x, int y, int *p, int *q, float  *s) ;
+int main()
+{
+ int x,y,sum,product; float aver;
+  scanf("%d%d", &x, &y);  
+  fun (x ,y, &sum,&product,&aver );
+  printf("%d,%d,%.1f", sum,product,aver);
+  return 0;
+}
+
+/* 请在这里填写答案 */
+void fun(int x, int y, int *p, int *q,float *s)
+{
+    *p=x+y;
+    *q=x*y;
+    *s=(x+y)/2.0;
+    return;
+}
